b9ccba57ec9563377b1a6d7353bf431b1c963f7e
Marc Steiner <git@marcsteiner.me>
2018-09-28 22:09:08 +0200

    Implement web-view as underlying UI framework

--- resources/index.js
+++ resources/index.js
@@ -1,6 +1,6 @@
 /*
  * kyo-rs - Rust rewrite of kyo, a modern osu! server switcher
- * Copyright (C) 2018 Marc3842h
+ * Copyright (C) 2018 Marc3842h, czapek
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published
@@ -14,24 +14,35 @@
  *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
-*/
+ */
 
-extern crate web_view;
+"use strict";
 
-static CONTENT: &'static str = include_str!("../resources/index.include.html");
-
-fn main() {
-    let init_cb = |_webview| {};
-    let frontend_cb = |_webview: &mut _, _arg: &_, _userdata: &mut _| {};
-    let user_data = ();
-    web_view::run(
-        "kyo-rs",
-        web_view::Content::Html(CONTENT),
-        Some((400, 260)),
-        false,
-        true,
-        init_cb,
-        frontend_cb,
-        user_data
+function connectShiro() {
+    invokeBackend(
+        {
+            cmd: "connect",
+            address: document.getElementById("connect-address").value
+        }
     );
+}
+
+function connectBancho() {
+    invokeBackend(
+        {
+            cmd: "disconnect"
+        }
+    )
+}
+
+function installCertificate() {
+    invokeBackend(
+        {
+            cmd: "install"
+        }
+    )
+}
+
+function invokeBackend(args) {
+    window.external.invoke(JSON.stringify(args));
 }
