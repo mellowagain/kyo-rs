5cd304bddd44f22f58bcb4f4d4da1b140d018b26
Marc Steiner <git@marcsteiner.me>
2018-09-28 21:11:29 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# kyo-rs
+
+The next generation of kyo, a fully featured and modern
+osu! server switcher - fully rewritten from ground up in Rust.
+
+![Screenshot](https://wontfix.club/i/DX2EFKJP.png)
---
+++ src/main.rs
@@ -0,0 +1,21 @@
+/*
+ * kyo - High quality and modern C++ osu! server switcher
+ * Copyright (C) 2018 Marc3842h, czapek
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+fn main() {
+    println!("Welcome to kyo-rs!");
+}
