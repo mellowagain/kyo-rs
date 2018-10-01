c174675be34d3fe7ce3f8bbe49ddba7ef81ff42a
Marc Steiner <git@marcsteiner.me>
2018-10-01 19:40:34 +0200

    Implement notifications for Windows 8 and Windows 10

--- src/platform_utils/win32.rs
+++ src/platform_utils/win32.rs
@@ -20,6 +20,9 @@ extern crate std;
 extern crate user32;
 extern crate winapi;
 extern crate winrt;
+extern crate winrt_notification;
+
+use winrt_notification::{Duration, Sound, Toast};
 
 pub fn is_root() -> bool {
     unsafe {
@@ -32,20 +35,10 @@ pub fn install_cert(cert: &str) {
 }
 
 pub fn send_notify(msg: &str) {
-    use winrt::*;
-    use winrt::windows::data::xml::dom::*;
-    use winrt::windows::ui::notifications::*;
-
-    unsafe {
-        let xml = ToastNotificationManager::get_template_content(ToastTemplateType_ToastText02).unwrap();
-        let elements = xml.get_elements_by_tag_name(&winrt::FastHString::new("text")).unwrap();
-
-        elements.item(0).unwrap().append_child(&*xml.create_text_node(&FastHString::from("kyo-rs")).unwrap().query_interface::<IXmlNode>().unwrap()).unwrap();
-        elements.item(1).unwrap().append_child(&*xml.create_text_node(&FastHString::from(msg)).unwrap().query_interface::<IXmlNode>().unwrap()).unwrap();
-
-        let toast = ToastNotification::create_toast_notification(&*xml).unwrap();
-        ToastNotificationManager::create_toast_notifier_with_id(
-            &winrt::FastHString::new(r#"{1AC14E77-02E7-4E5D-B744-2EB1AE5198B7}\WindowsPowerShell\v1.0\powershell.exe"#)
-        ).unwrap().show(&*toast).unwrap();
-    }
+    Toast::new(Toast::POWERSHELL_APP_ID)
+        .title("kyo-rs")
+        .text1(msg)
+        .duration(Duration::Short)
+        .show()
+        .unwrap();
 }
